610e36c20910f82704d0f8ed2f4fce290a7c1135
Juan Carlos Brenes-Torres <15621813+jcbrenes@users.noreply.github.com>
2020-08-11 18:34:28 -0600

    Major update al código
    Se sacan funcioner serial y wire de las interrupciones
    Se cambia el movimiento del servo con delay a uso de millis
    Se actualiza la distancia mínima del sharp, el ángulo máximo del servo y el valor de tiempo entre movimientos

--- Stm32_combo_v2.ino
+++ Stm32_combo_v2.ino
@@ -9,7 +9,6 @@ Códigos de evento:
   4)IR izquierdo
   5)Temperatura
   6)Batería baja
-
 Adicionalmente tiene tres salidas conectadas a leds para notificación y un switch extra
 */
 
@@ -36,11 +35,11 @@ Servo myServo;
 
 //Constantes de configuración//
 const float beta=0.1;  //Constante para el filtro, ajustar para cambiar el comportamiento
-const int servoDelay=15; //Tiempo entre cada paso del servo en milisegundos
-const float distanciaMinimaSharp=5; //Distancia mínima en milimetros para detección de obstáculos
+const int servoDelay=10; //Tiempo entre cada paso del servo en milisegundos
+const float distanciaMinimaSharp=130; //Distancia mínima en milimetros para detección de obstáculos
 const int debounceTime=400; //Debounce para sensor IR frontal
 const float maxTemp=40; //Temperatura de detección de fuego
-const int movimientoMaximo=120; //Maxima rotacion en grados realizada por el servo que rota el sharp
+const int movimientoMaximo=160; //Maxima rotacion en grados realizada por el servo que rota el sharp
 
 //Variables globales//
 int angulo=0;
@@ -48,7 +47,10 @@ int lectura=0;
 float distancia=0;
 float filtrado=0;
 int cuentaBateria=0;
-
+int sensorIRdetectado=0;
+unsigned long millisAnterior=0;
+int servoPos=0;
+int incremento=1; //cantidad de grados que aumenta el servo en cada movimiento
 
 
 void setup() {
@@ -76,39 +78,36 @@ void setup() {
   Wire.begin();           //Inicia comunicación i2c como master
   Serial1.begin(115200);  //Inicia la comunicación serial a 115200 baud
   Serial1.println("On");  //Enviar un "on" por el serial para confirmar que el setup se ejecutó correctamente
+
 }
  
 void loop() {
-  moverServo();
-  revisarTemperatura();
-  revisarBateria();
+  //moverServo();
+  revisarSharp();
+  revisarSensoresIR();
+  //revisarTemperatura(); //desactivado mientras no está soldado el sensor al PCB
+  //revisarBateria();
 }
 
-void moverServo(){ //Mueve el servo un grado y mide la distancia
-  for(int i=0;i<movimientoMaximo;i+=1){
-    myServo.write(i);
-    angulo=i;
-    delay(servoDelay);
+void moverServo(){ //Mueve el servo un valor determinado cada cierto tiempo
+  if((millis() - millisAnterior) > servoDelay) {  //Se utiliza la función millis para evitar usar los delays
+    millisAnterior = millis();
+    servoPos += incremento;
+    myServo.write(servoPos);
+    angulo= 90 - servoPos; //se considera 0 grados, justo al frente
+    if ((servoPos >= movimientoMaximo) || (servoPos <= 0)){ // se terminó el semiciclo, se invierte la dirección
+      incremento = -incremento;
+    }
+  }
+}
+
+void revisarSharp(){ //Revisa el valor del sensor Sharp
     lectura=analogRead(sharp);
     filtrado=filtrado-(beta*(filtrado-lectura)); //Filtro de media movil
     distancia=1274160*pow(filtrado,-1.174); //Regresión lineal de datos experimentales para obtener distancia en milimetros
-    //Serial1.println(distancia);
     if (distancia<distanciaMinimaSharp){
       enviarDato(1,int(distancia),angulo); //Enviar obstaculo tipo 1 con distancia en cm y angulo
     }
-  }
-  for(int i=movimientoMaximo;i>0;i-=1){
-    myServo.write(i);
-    angulo=i;
-    delay(servoDelay);
-    lectura=analogRead(sharp);
-    filtrado=filtrado-(beta*(filtrado-lectura));
-    distancia=1274160*pow(filtrado,-1.174);
-    //Serial1.println(distancia);
-    if (distancia<distanciaMinimaSharp){
-      enviarDato(1,int(distancia),angulo); //Enviar obstaculo tipo 1 con distancia en cm y angulo
-    }
-  }
 }
 
 void revisarTemperatura(){ //Lectura del sensor de temperatura
@@ -126,10 +125,34 @@ void revisarTemperatura(){ //Lectura del sensor de temperatura
 void revisarBateria(){ //Revisar la batería cada 20 ciclos del loop, codigo 6
   if(digitalRead(lowBattery)){
     cuentaBateria++;
-    if(cuentaBateria>=20){ //Si la bateria esta baja enviar advertencia cada 20 ciclos del loop principal
+    if(cuentaBateria>=2000){ //Si la bateria esta baja enviar advertencia cada 2000 ciclos del loop principal
       enviarDato(6,0,0);
       cuentaBateria=0;
     }
+  }
+}
+
+void revisarSensoresIR(){ //Revisa si la variable de sensores IR cambio de estado en un interupción, en ese caso envía el dato
+//Esto es porque los serial y wire dentro de interrupciones dan problema
+
+  switch (sensorIRdetectado) {
+    case 0:    // no se ha detectado nada
+      break;
+    case 2:    // sensor IR frontal
+      Serial1.print("F ");
+      enviarDato(2,0,0);
+      sensorIRdetectado=0;
+      break;
+    case 3:    // sensor IR inferior derecho
+      Serial1.print("R ");
+      enviarDato(3,0,0);
+      sensorIRdetectado=0;
+      break;
+    case 4:    // sensor IR inferior izquierdo
+      Serial1.print("L ");
+      enviarDato(4,0,0);
+      sensorIRdetectado=0;
+      break;
   }
 }
 
@@ -139,6 +162,7 @@ void enviarDato(int caso, int distancia, int angulo){ //Envia el paquete de dato
   digitalWrite(int1,LOW);
   char dato[50];
   sprintf(dato, "%d,%d,%d.", caso, distancia, angulo); //Genera un string para la transmisión
+  Serial1.println(dato);
   Wire.beginTransmission(42);           //Comienza transmisión al esclavo 42 (Feather)
   Wire.write(dato);                     //Envia el valor al esclavo
   Wire.endTransmission();               //Detener la transmisión
@@ -154,20 +178,16 @@ void irFrontalSend(){ //Interrupcion para sensor frontal, codigo 2
   if (interrupt_time - last_interrupt_time > debounceTime) //Debounce para el sensor frontal
   {
     if(!digitalRead(irFrontal)){
-      enviarDato(2,0,0);
-      Serial1.println("F");
+      sensorIRdetectado=2;
     }
-      
   }
   last_interrupt_time = interrupt_time;
 }
 
 void irRightSend(){ //Interrupcion para sensor derecho, codigo 3
-  enviarDato(3,0,0);
-  Serial1.println("R");
+  sensorIRdetectado=3;
 }
 
 void irLeftSend(){ //Interrupcion para sensor izquierdo, codigo 4
-  enviarDato(4,0,0);
-  Serial1.println("L");
+  sensorIRdetectado=4;
 }
