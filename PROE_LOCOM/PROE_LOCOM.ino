a176d200400f716bdc116a7090b72d11d6da6971
Isaac <isaacfelipe2007@gmail.com>
2023-03-29 10:45:50 -0600

    Probando commit a nueva rama

--- PROE_LOCOM/PROE_LOCOM.ino
+++ PROE_LOCOM/PROE_LOCOM.ino
@@ -20,7 +20,7 @@
 #endif
 
 //Variables del enjambre para la comunicación a la base
-const uint8_t cantidadRobots = 4; //Cantidad de robots en enjambre. No cuenta la base, solo los que hablan.
+const uint8_t cantidadRobots = 3; //Cantidad de robots en enjambre. No cuenta la base, solo los que hablan.
 unsigned long idRobot = 2; //ID del robot, este se usa para ubicar al robot dentro de todo el ciclo de TDMA.
 
 // Variables para el proceso de arranque y transformación de coordenadas
